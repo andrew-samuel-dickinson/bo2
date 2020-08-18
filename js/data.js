ba3d2747d3c21e694e36457bec11872638fac977
andrew-samuel-dickinson <andrew.samuel.dickinson@gmail.com>
2020-08-18 15:18:20 +0100

    Edits for Bo

--- js/data.js
+++ js/data.js
@@ -1,20 +1,13 @@
 // TODO move this to JSON?
 myQaData = [
-    {"Q" : "What is 5 x 11", "A" : "55"},
-    {"Q" : "What is 8 x 11", "A" : "88"},
-    {"Q" : "What is 11 x 8", "A" : "88"},
-    {"Q" : "What is 8 x 9", "A" : "72"},
-    {"Q" : "What is 9 x 9", "A" : "81"},
-    {"Q" : "What is 5 x 5", "A" : "25"},
-    {"Q" : "What is 6 x 6", "A" : "36"},
-    {"Q" : "What is 7 x 7", "A" : "49"},
-    {"Q" : "What is the missing number: 9, 18, 27, ??, 45", "A" : "36"},
-    {"Q" : "What is the missing number: 1, 4, 7, ??, 13, 16", "A" : "10"},
-    {"Q" : "What is the missing number: 54, 45, 36, ??, 18, 9", "A" : "27"},
-    {"Q" : "What is the missing number: 2, 4, 8, ??", "A" : "16"},
-    {"Q" : "What is 13 x 10", "A" : "130"},
-    {"Q" : "What is 13 x 11", "A" : "143"},
-    {"Q" : "I am 4 years older than my brother. I am also twice his age. How old am I?", "A" : "8"},
+    {"Q" : "5 + 5", "A" : "10"},
+    {"Q" : "10 + 10", "A" : "20"},
+    {"Q" : "10 + 2", "A" : "12"},
+    {"Q" : "2 + 10", "A" : "12"},
+    {"Q" : "10 + 5", "A" : "15"},
+    {"Q" : "5 + 10", "A" : "15"},
+    {"Q" : "10 + 8", "A" : "18"},
+    {"Q" : "8 + 10", "A" : "18"},
 ];
 
 images = 
@@ -22,7 +15,9 @@ images =
     "https://i.pinimg.com/236x/70/dd/52/70dd5208032fdcac975727e4d1484c18--funny-stuff-so-funny.jpg",
     "https://i.insider.com/5c79a8cfeb3ce837863155f5?width=750&format=jpeg&auto=webp",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6YDBxxgiWarH-2u2chH1riSbd5aHnG0tJBw&usqp=CAU", 
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRphb761m4liOHpIMdxOVkPrP7IEq69JQib1g&usqp=CAU"
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRphb761m4liOHpIMdxOVkPrP7IEq69JQib1g&usqp=CAU",
 ];
 
-video = "https://www.youtube.com/embed/RW61RQZojMQ?autoplay=1";
+video = "https://www.youtube.com/embed/BjILD_ajfYk?autoplay=1";
+
+//<iframe width="560" height="315" src="https://www.youtube.com/embed/fKopy74weus" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
