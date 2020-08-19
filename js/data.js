7ee91c58518d013931d8b13e28aeb2344bf28b27
andrew-samuel-dickinson <andrew.samuel.dickinson@gmail.com>
2020-08-19 10:15:42 +0100

    Updated questions and links

--- js/data.js
+++ js/data.js
@@ -1,24 +1,24 @@
 // TODO move this to JSON?
 myQaData = [
-    {"Q" : "5 + 5", "A" : "10"},
-    {"Q" : "10 + 10", "A" : "20"},
-    {"Q" : "10 + 2", "A" : "12"},
-    {"Q" : "2 + 10", "A" : "12"},
-    {"Q" : "10 + 5", "A" : "15"},
-    {"Q" : "5 + 10", "A" : "15"},
-    {"Q" : "10 + 8", "A" : "18"},
-    {"Q" : "8 + 10", "A" : "18"},
+    {"Q" : "2 + ?? = 3", "A" : "1"},
+    {"Q" : "3 - 2", "A" : "1"},
+    {"Q" : "2 + ?? = 4", "A" : "2"},
+    {"Q" : "4 - 2", "A" : "2"},    
+    {"Q" : "5 + ?? = 10", "A" : "5"},
+    {"Q" : "10 - 5", "A" : "5"},
+    {"Q" : "10 + ?? = 12", "A" : "2"},
+    {"Q" : "12 -10", "A" : "2"},    
 ];
 
 images = 
 [
-    "https://i.pinimg.com/236x/70/dd/52/70dd5208032fdcac975727e4d1484c18--funny-stuff-so-funny.jpg",
+    "https://i0.wp.com/bestlifeonline.com/wp-content/uploads/2018/04/Animal-jokes-funny-sheep.jpg?resize=1024%2C683&ssl=1",
     "https://i.insider.com/5c79a8cfeb3ce837863155f5?width=750&format=jpeg&auto=webp",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS6YDBxxgiWarH-2u2chH1riSbd5aHnG0tJBw&usqp=CAU", 
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRphb761m4liOHpIMdxOVkPrP7IEq69JQib1g&usqp=CAU"
+    "https://piximus.net/media2/58686/funny-animals-61-2.jpg", 
+    "https://www.abc.net.au/cm/rimage/11514316-4x3-xlarge.jpg?v=3"
 ];
 
-video_id = "fKopy74weus";
+video_id = "VhQXUZZS_4c";
 video = "https://www.youtube.com/embed/" + video_id + "?autoplay=1";
 
 default_image = "http://placehold.it/800x500"
